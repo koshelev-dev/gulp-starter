268dda2544b45c122d207bf241ad2a88005d265e
Nikita Koshelev <koshelevn92@gmail.com>
2020-09-24 18:17:47 +0300

    Setting main gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+const srcDir = 'source'
+const imagesWatch  = 'jpg,jpeg,png' // List of images extensions for watching & compression (comma separated)
+const filesWatch   = 'html,json,md,woff2' // List of files extensions for watching & hard reload (comma separated)
+
+const paths = {
+  scripts: {
+    src: [
+      // Connect libraries and packages javascript here
+      // 'node_modules/jquery/dist/jquery.min.js',  npm i -S jquery
+      `${srcDir}/js/main.js` // Always at the end
+    ],
+    dest: `${srcDir}/js`
+  },
+  styles: {
+    src: `${srcDir}/scss/main.scss`,
+    dest: `${srcDir}/css`
+  },
+  images: {
+    src: `${srcDir}/images/src`,
+    dest: `${srcDir}/images`
+  },
+}
+
+const { src, watch, dest, series, parallel } = require('gulp')
+const concat        = require('gulp-concat')
+const browserSync   = require('browser-sync').create()
+const uglify        = require('gulp-uglify-es').default
+const sass          = require('gulp-sass')
+const groupMediaCSS = require('gulp-group-css-media-queries')
+const autoprefixer  = require('gulp-autoprefixer')
+const cleanCSS      = require('gulp-clean-css')
+const newer         = require('gulp-newer')
+const webp          = require('gulp-webp')
+const imagemin      = require('gulp-imagemin')
+const del           = require('del')
+
+function browsersync() {
+  browserSync.init({
+    server: { baseDir: `${srcDir}/` },
+    notify: false
+  })
+}
+
+function scripts() {
+  return src(paths.scripts.src)
+  .pipe(concat('scripts.min.js'))
+  .pipe(uglify())
+  .pipe(dest(paths.scripts.dest))
+  .pipe(browserSync.stream())
+}
+
+function styles() {
+  return src(paths.styles.src)
+  .pipe(sass())
+  .pipe(groupMediaCSS())
+  .pipe(autoprefixer({ overrideBrowserslist: ['last 5 versions'], grid: true }))
+  .pipe(concat('style.min.css'))
+  .pipe(cleanCSS({ level: { 1: { specialComments: 0 } }, }))
+  .pipe(dest(paths.styles.dest))
+  .pipe(browserSync.stream())
+
+}
+
+function images() {
+  return src(`${paths.images.src}/**/*`)
+  .pipe(newer(paths.images.dest))
+  .pipe(webp({ quality: 70 }))
+  .pipe(dest(paths.images.dest))
+  .pipe(src(`${paths.images.src}/**/*`))
+  .pipe(newer(paths.images.dest))
+  .pipe(imagemin({ progressive: true, svgPlugins: [{ removeViewBox: false }], interlaced: true, optimizationLevel: 3 }))
+  .pipe(dest(paths.images.dest))
+  .pipe(browserSync.stream())
+}
+
+function cleanimg() {
+  return del([`${paths.images.dest}/**/*`, `!${paths.images.src}`], { force: true })
+}
+
+function startwatch(){
+  watch(`${srcDir}/**/*.{${filesWatch}}`).on('change', browserSync.reload);
+  watch([`${srcDir}/js/*.js`, `!${paths.scripts.dest}/*.min.js`], scripts);
+  watch(`${srcDir}/scss/**/*`, styles);
+  watch(`${paths.images.src}/**/*.{${imagesWatch}}`, images);
+}
+
+exports.scripts = scripts
+exports.browsersync = browsersync
+exports.styles = styles
+exports.images = images
+exports.cleanimg = cleanimg
+
+exports.build = series(cleanimg, scripts, styles, images)
+exports.default = parallel(scripts, styles, images, browsersync,startwatch)
