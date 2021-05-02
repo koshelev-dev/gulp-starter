281c4db53822bbe5201340bc5d6590b8ecdd172e
Nikita Koshelev <koshelevn92@gmail.com>
2021-05-02 19:25:14 +0300

    update layout njk

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,11 +3,11 @@ import nunjucks from 'gulp-nunjucks-templates';
 import config from '../config';
 
 export const htmlBuilds = () => (
-  gulp.src(`${config.src.view}/pages/*.html`)
+  gulp.src(`${config.src.view}/pages/*.njk`)
     .pipe(nunjucks({
       path: config.src.view,
     }))
     .pipe(gulp.dest(config.dest.root))
 );
 
-export const htmlWatch = () => gulp.watch(`${config.src.view}/**/*.html`, htmlBuilds);
+export const htmlWatch = () => gulp.watch(`${config.src.view}/**/*.njk`, htmlBuilds);
