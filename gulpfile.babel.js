ac143768ceec650719bd9842a933d8c290e1f2a3
Nikita Koshelev <koshelevn92@gmail.com>
2021-05-02 11:24:04 +0300

    add task scripts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,20 @@
 import gulp from 'gulp';
+import config from './gulp/config';
 import clean from './gulp/tasks/clean';
 import server from './gulp/tasks/server';
-import config from './gulp/config';
+import { scriptBuild, scriptWatch } from './gulp/tasks/scripts';
 
 config.setEnv();
 
 export const build = gulp.series(
   clean,
+  scriptBuild,
 );
 
 export const watch = gulp.series(
-  build,
+  // build,
   server,
+  scriptWatch,
 );
 
 // const srcDir = 'source';
